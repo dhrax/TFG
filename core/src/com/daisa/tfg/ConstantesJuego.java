20f4ea9c06c59da4b01b2ae4d8f275a32c978792
Dhrax <davidflpc@gmail.com>
2020-05-22 13:18:42 +0200

    Municion y recarga automática. HUD terminado.

--- core/src/com/daisa/tfg/ConstantesJuego.java
+++ core/src/com/daisa/tfg/ConstantesJuego.java
@@ -10,4 +10,8 @@ public class ConstantesJuego {
     public static float ALTO_UNIDADES = 20;
     public static float PPU = ALTO_PANTALLA / ALTO_UNIDADES;
     public static float ANCHO_UNIDADES = ANCHO_PANTALLA / PPU;
+
+    public static int MUNICION_MAXIMA = 10;
+    public static int TIEMPO_REGENERACION_BALAS_MILIS = 3000;
+    public static int CADENCIA_DISPAROS_MILIS = 1000;
 }
